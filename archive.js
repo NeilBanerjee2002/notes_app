118c4195d79720e7daedd435b6973ef53cdbec1d
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2025-04-26 18:20:08 +0530

    added title display functionality to archive screen

--- archive.js
+++ archive.js
@@ -1,36 +1,50 @@
 import { renderNotes } from "./app.js";
 
+let title = document.querySelector(".archive-notes");
 let archivedNotes = document.querySelector(".archive-notes-container");
+
+let arrayOfNotes = JSON.parse(localStorage.getItem("notes"));
 
 archivedNotes.innerHTML = renderNotes(
     JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
 );
 
+function toggleTitles(arrayOfNotes){
+    if(arrayOfNotes.some(note => note.isArchived == true)){
+        title.classList.remove("d-none"); // show
+    }
+    else{
+        title.classList.add("d-none"); // hide
+    }
+}
+
+
 archivedNotes.addEventListener("click", (event) => {
     switch(event.target.dataset.type) {
         case "delete":
-            let arrayOfNotes = JSON.parse(localStorage.getItem("notes"));
             arrayOfNotes = arrayOfNotes.filter((note) => note.id != event.target.dataset.id);
             localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
             archivedNotes.innerHTML = renderNotes(
                 JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
             );
+            toggleTitles(arrayOfNotes)
             break;
         case "archive":
-            let notes = JSON.parse(localStorage.getItem("notes"));
-            notes = notes.map((note) => note.id == event.target.dataset.id ? {...note, isArchived: !note.isArchived} : note);
-            localStorage.setItem("notes", JSON.stringify(notes));
+            arrayOfNotes = arrayOfNotes.map((note) => note.id == event.target.dataset.id ? {...note, isArchived: !note.isArchived} : note);
+            localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
             archivedNotes.innerHTML = renderNotes(
                 JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
             );
+            toggleTitles(arrayOfNotes)
             break;
         case "pinned":
-            let notesArray = JSON.parse(localStorage.getItem("notes"));
-            notesArray = notesArray.map((note) => note.id == event.target.dataset.id ? {...note, isPinned: !note.isPinned} : note);
-            localStorage.setItem("notes", JSON.stringify(notesArray));
+            arrayOfNotes = arrayOfNotes.map((note) => note.id == event.target.dataset.id ? {...note, isPinned: !note.isPinned} : note);
+            localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
             archivedNotes.innerHTML = renderNotes(
                 JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
             );
+            toggleTitles(arrayOfNotes)
             break;    
     }
 });
+toggleTitles(arrayOfNotes)
