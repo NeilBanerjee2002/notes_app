ba4f8a280ae9d22c01db391ef9191c84d5ce9466
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2025-04-26 18:00:06 +0530

    button functionality for archived page added

--- archive.js
+++ archive.js
@@ -6,3 +6,31 @@ archivedNotes.innerHTML = renderNotes(
     JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
 );
 
+archivedNotes.addEventListener("click", (event) => {
+    switch(event.target.dataset.type) {
+        case "delete":
+            let arrayOfNotes = JSON.parse(localStorage.getItem("notes"));
+            arrayOfNotes = arrayOfNotes.filter((note) => note.id != event.target.dataset.id);
+            localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
+            archivedNotes.innerHTML = renderNotes(
+                JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
+            );
+            break;
+        case "archive":
+            let notes = JSON.parse(localStorage.getItem("notes"));
+            notes = notes.map((note) => note.id == event.target.dataset.id ? {...note, isArchived: !note.isArchived} : note);
+            localStorage.setItem("notes", JSON.stringify(notes));
+            archivedNotes.innerHTML = renderNotes(
+                JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
+            );
+            break;
+        case "pinned":
+            let notesArray = JSON.parse(localStorage.getItem("notes"));
+            notesArray = notesArray.map((note) => note.id == event.target.dataset.id ? {...note, isPinned: !note.isPinned} : note);
+            localStorage.setItem("notes", JSON.stringify(notesArray));
+            archivedNotes.innerHTML = renderNotes(
+                JSON.parse(localStorage.getItem("notes")).filter((note) => note.isArchived == true)
+            );
+            break;    
+    }
+});
